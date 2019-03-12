55e64233d537eeda5c9cb126a324a190dce63dab
Sharaj Panwar <ijz121@my.utsa.edu>
2019-03-12 00:26:46 -0500

    Add files via upload

---
+++ keras-model/AttentionWithContext.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from keras.layers import Layer
+from keras import backend as K, initializers, regularizers, constraints
+
+class AttentionWithContext(Layer):
+    """
+        Attention operation, with a context/query vector, for temporal data.
+        Supports Masking.
+        Follows the work of Yang et al. [https://www.cs.cmu.edu/~diyiy/docs/naacl16.pdf]
+        "Hierarchical Attention Networks for Document Classification"
+        by using a context vector to assist the attention
+        # Input shape
+            3D tensor with shape: `(samples, steps, features)`.
+        # Output shape
+            2D tensor with shape: `(samples, features)`.
+        :param kwargs:
+        Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
+        The dimensions are inferred based on the output shape of the RNN.
+        Example:
+            model.add(LSTM(64, return_sequences=True))
+            model.add(AttentionWithContext())
+        """
+
+    def __init__(self, init='glorot_uniform', kernel_regularizer=None, bias_regularizer=None, kernel_constraint=None, bias_constraint=None,  **kwargs):
+        self.supports_masking = True
+        self.init = initializers.get(init)
+        self.kernel_initializer = initializers.get('glorot_uniform')
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+
+        super(AttentionWithContext, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight((input_shape[-1], 1),
+                                 initializer=self.kernel_initializer,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.kernel_regularizer,
+                                 constraint=self.kernel_constraint)
+        self.b = self.add_weight((input_shape[1],),
+                                 initializer='zero',
+                                 name='{}_b'.format(self.name),
+                                 regularizer=self.bias_regularizer,
+                                 constraint=self.bias_constraint)
+
+        self.u = self.add_weight((input_shape[1],),
+                                 initializer=self.kernel_initializer,
+                                 name='{}_u'.format(self.name),
+                                 regularizer=self.kernel_regularizer,
+                                 constraint=self.kernel_constraint)
+        self.built = True
+
+    def compute_mask(self, input, mask):
+        return None
+
+    def call(self, x, mask=None):
+        # (x, 40, 300) x (300, 1)
+        multData =  K.dot(x, self.kernel) # (x, 40, 1)
+        multData = K.squeeze(multData, -1) # (x, 40)
+        multData = multData + self.b # (x, 40) + (40,)
+
+        multData = K.tanh(multData) # (x, 40)
+
+        multData = multData * self.u # (x, 40) * (40, 1) => (x, 1)
+        multData = K.exp(multData) # (X, 1)
+
+        # apply mask after the exp. will be re-normalized next
+        if mask is not None:
+            mask = K.cast(mask, K.floatx()) #(x, 40)
+            multData = mask*multData #(x, 40) * (x, 40, )
+
+        # in some cases especially in the early stages of training the sum may be almost zero
+        # and this results in NaN's. A workaround is to add a very small positive number ε to the sum.
+        # a /= K.cast(K.sum(a, axis=1, keepdims=True), K.floatx())
+        multData /= K.cast(K.sum(multData, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+        multData = K.expand_dims(multData)
+        weighted_input = x * multData
+        return K.sum(weighted_input, axis=1)
+
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[-1],)
---
+++ keras-model/trial_with_atten_.py
@@ -0,0 +1,64 @@
+from AttentionWithContext import AttentionWithContext
+import numpy as np
+from keras.layers import Dense, Dropout, Lambda
+from keras.layers import LSTM, TimeDistributed, Reshape
+import h5py
+from keras.engine import Input, Model
+from keras.optimizers import Adam
+from sklearn.metrics import roc_auc_score
+
+# X_train = h5py.File('tr_drive_hrnn_1.mat')['tr_drive_hrnn_1']
+# X_train = np.transpose(X_train) #0.125 sec data
+# print(X_train.shape)
+#
+# X_test = h5py.File('te_drive_hrnn_1.mat')['te_drive_hrnn_1']
+# X_test = np.transpose(X_test)
+# print(X_test.shape)
+#
+# Y_train = np.zeros(X_train.shape[0])
+# Y_test = np.zeros(X_train.shape[0])
+# Y_train[0:500] = 1
+# Y_test[0:250] = 1
+
+def hrnn_model():
+
+    input_section = Input(shape=(4096, ), dtype='float32') # 0.5 second flattened
+    epoch_section = Reshape((64, 64), input_shape=(4096, ))(input_section) # 0.5 second epoch section
+    lstm1 = LSTM(32, return_sequences=True, name='recurrent_layer')(epoch_section)
+    dense1 = TimeDistributed(Dense(32))(lstm1)
+    # TimeDistributed to apply a Dense layer to each of the 32 timesteps, independently
+    attention = AttentionWithContext()(dense1) #apply attention
+    model_section = Model(inputs=input_section, outputs=attention)
+    model_section.summary() # summary of LSTM model applied on each 0.5 second of a 5 sec epoch
+
+    ##################################################################################################################
+
+    model_input = Input(shape=(10, 4096), dtype='float32')
+    section_output = TimeDistributed(model_section)(model_input)
+    # "TimeDistributed layer"for sequentially feeding each 0.5 second of 5 second EEG epoch,(each section of 10 sections of 4096)
+    lstm2 = LSTM(32, return_sequences=True)(section_output)
+    lstm3 = TimeDistributed(Dense(32))(lstm2)
+    attention_2 = AttentionWithContext()(lstm3)
+    model_output = Dense(1, activation='sigmoid')(attention_2)
+
+    model = Model(inputs=model_input, outputs=model_output)
+    optimizer = Adam(lr=0.01)
+
+    print('Compiling...')
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizer,
+                  metrics=['accuracy'])
+
+    print("model fitting - Hierachical LSTM")
+    model.summary()
+    return model
+
+if __name__ == '__main__':
+    model = hrnn_model()
+    # model.fit(X_train, Y_train, batch_size=128, epochs=10, validation_split=0.1, validation_data=(X_train, Y_train))
+    # score, acc = model.evaluate(X_test, Y_test, batch_size=128)
+    # print('Test accuracy:', acc)
+    # target = model.predict(X_test, batch_size=128)
+    # auc = roc_auc_score(Y_test, target)
+    # print(auc)
+
